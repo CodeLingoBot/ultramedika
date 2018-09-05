dd3641aa19ee9401af312869e28dadb8e57ab101
mudroljub <mudroljub@gmail.com>
2018-09-05 13:38:54 +0200

    preimenovane slike

--- galerija.php
+++ galerija.php
@@ -15,27 +15,27 @@ img {
   <h1>Galerija</h1>
 
   <div>
-    <a href="slike/velike-slike/macak22.jpg" target="_self"><img src="slike/male-slike/poni22.jpg";id="red1" /></a>
-    <a href="slike/velike-slike/macak21.jpg" target="_self"><img src="slike/male-slike/poni21.jpg";id="red1" /></a>
-    <a href="slike/velike-slike/macak23.jpg" target="_self"><img src="slike/male-slike/poni23.jpg" ;id="red1"/></a>
-    <a href="slike/velike-slike/macak24.jpg" target="_self"><img src="slike/male-slike/poni24.jpg";id="red2" /></a>
-    <a href="slike/velike-slike/macak25.jpg" target="_self"><img src="slike/male-slike/poni25.jpg";id="red2" /></a>
-    <a href="slike/velike-slike/macak18.jpg" target="_self"><img src="slike/male-slike/poni18.jpg" ;id="red2"/></a>
-    <a href="slike/velike-slike/macak17.jpg" target="_self"><img src="slike/male-slike/poni17.jpg";id="red3" /></a>
-    <a href="slike/velike-slike/macak15.jpg" target="_self"><img src="slike/male-slike/poni15.jpg" ;id="red3"/></a>
-    <a href="slike/velike-slike/macak13.jpg" target="_self"><img src="slike/male-slike/poni13.jpg" ;id="red3"/></a>
-    <a href="slike/velike-slike/macak1.jpg" target="_self"><img src="slike/male-slike/poni1.jpg";id="red4" /></a>
-    <a href="slike/velike-slike/macak4.jpg" target="_self"><img src="slike/male-slike/poni4.jpg";id="red4" /></a>
-    <a href="slike/velike-slike/macak5.jpg" target="_self"><img src="slike/male-slike/poni5.jpg" ;id="red4"/></a>
-    <a href="slike/velike-slike/macak2.jpg" target="_self"><img src="slike/male-slike/poni2.jpg";id="red5" /></a>
-    <a href="slike/velike-slike/macak10.jpg" target="_self"><img src="slike/male-slike/poni10.jpg";id="red5" /></a>
-    <a href="slike/velike-slike/macak3.jpg" target="_self"><img src="slike/male-slike/poni3.jpg" ;id="red5"/></a>
-    <a href="slike/velike-slike/macak6.jpg" target="_self"><img src="slike/male-slike/poni6.jpg" ;id="red6"/></a>
-    <a href="slike/velike-slike/macak11.jpg" target="_self"><img src="slike/male-slike/poni11.jpg";id="red6" /></a>
-    <a href="slike/velike-slike/macak7.jpg" target="_self"><img src="slike/male-slike/poni7.jpg" ;id="red6"/></a>
-    <a href="slike/velike-slike/macak9.jpg" target="_self"><img src="slike/male-slike/poni9.jpg" ;id="red7"/></a>
-    <a href="slike/velike-slike/macak16.jpg" target="_self"><img src="slike/male-slike/poni16.jpg";id="red7" /></a>
-    <a href="slike/velike-slike/macak14.jpg" target="_self"><img src="slike/male-slike/poni14.jpg" ;id="red7"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika22.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika22.jpg";id="red1" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika21.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika21.jpg";id="red1" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika23.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika23.jpg" ;id="red1"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika24.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika24.jpg";id="red2" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika25.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika25.jpg";id="red2" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika18.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika18.jpg" ;id="red2"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika17.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika17.jpg";id="red3" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika15.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika15.jpg" ;id="red3"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika13.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika13.jpg" ;id="red3"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika1.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika1.jpg";id="red4" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika4.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika4.jpg";id="red4" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika5.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika5.jpg" ;id="red4"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika2.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika2.jpg";id="red5" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika10.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika10.jpg";id="red5" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika3.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika3.jpg" ;id="red5"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika6.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika6.jpg" ;id="red6"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika11.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika11.jpg";id="red6" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika7.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika7.jpg" ;id="red6"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika9.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika9.jpg" ;id="red7"/></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika16.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika16.jpg";id="red7" /></a>
+    <a href="slike/velike-slike/ordinacija-ultramedika14.jpg" target="_self"><img src="slike/male-slike/ordinacija-ultramedika14.jpg" ;id="red7"/></a>
   </div>
 </div>
 
